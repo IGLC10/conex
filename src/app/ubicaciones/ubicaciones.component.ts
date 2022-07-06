101096c2df51a6eb18376f3fc46141e26607428c
IGLC10 <isaacgerardo12345@gmail.com>
2022-07-06 17:54:49 -0600

    Version 1.4
    Ubicaciones funcionando (Json,, ngFor y ngIf)

--- src/app/ubicaciones/ubicaciones.component.ts
+++ src/app/ubicaciones/ubicaciones.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UbicacionesComponent implements OnInit {
 
+  json = [
+    {temp: 31, humedad: 10, sensor: "Aula 22"},
+    {temp: 27, humedad: 12, sensor: "Cafeteria"},
+    {temp: 33, humedad: 15, sensor: "Laboratorio"},
+    {temp: 32, humedad: 11, sensor: "Aula 21"}
+  ]
+  valor="";
+
   constructor() { }
 
   ngOnInit(): void {
